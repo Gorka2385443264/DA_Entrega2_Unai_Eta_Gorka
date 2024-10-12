27a5098d0e8dd3c4bac88792ac7769605343f638
unknown <unaicaminos186@gmail.com>
2024-10-12 21:50:51 +0200

    textBoxa kendu

--- DA_Entrega2_Unai_Eta_Gorka_C#/DA_Entrega2_Unai_Eta_Gorka/Form2.Designer.cs
+++ DA_Entrega2_Unai_Eta_Gorka_C#/DA_Entrega2_Unai_Eta_Gorka/Form2.Designer.cs
@@ -30,28 +30,24 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.btnGehitu = new System.Windows.Forms.Button();
-            this.txtId = new System.Windows.Forms.TextBox();
             this.txtIzena = new System.Windows.Forms.TextBox();
             this.txtAbizena = new System.Windows.Forms.TextBox();
             this.txtTel = new System.Windows.Forms.TextBox();
             this.txtNan = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.txtIdLangilea = new System.Windows.Forms.TextBox();
-            this.txtIdErabiltzailea = new System.Windows.Forms.TextBox();
             this.btnGehitu2 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(134, 102);
+            this.label1.Location = new System.Drawing.Point(134, 112);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(98, 16);
             this.label1.TabIndex = 0;
@@ -67,13 +63,6 @@
             this.btnGehitu.Text = "Gehitu";
             this.btnGehitu.UseVisualStyleBackColor = true;
             this.btnGehitu.Click += new System.EventHandler(this.btnGehitu_Click);
-            // 
-            // txtId
-            // 
-            this.txtId.Location = new System.Drawing.Point(165, 127);
-            this.txtId.Name = "txtId";
-            this.txtId.Size = new System.Drawing.Size(100, 22);
-            this.txtId.TabIndex = 2;
             // 
             // txtIzena
             // 
@@ -119,32 +108,15 @@
             this.txtIdLangilea.Size = new System.Drawing.Size(100, 22);
             this.txtIdLangilea.TabIndex = 8;
             // 
-            // txtIdErabiltzailea
-            // 
-            this.txtIdErabiltzailea.Location = new System.Drawing.Point(473, 155);
-            this.txtIdErabiltzailea.Name = "txtIdErabiltzailea";
-            this.txtIdErabiltzailea.Size = new System.Drawing.Size(100, 22);
-            this.txtIdErabiltzailea.TabIndex = 9;
-            // 
             // btnGehitu2
             // 
-            this.btnGehitu2.Location = new System.Drawing.Point(409, 195);
+            this.btnGehitu2.Location = new System.Drawing.Point(426, 179);
             this.btnGehitu2.Name = "btnGehitu2";
             this.btnGehitu2.Size = new System.Drawing.Size(75, 23);
             this.btnGehitu2.TabIndex = 10;
             this.btnGehitu2.Text = "Gehitu";
             this.btnGehitu2.UseVisualStyleBackColor = true;
             this.btnGehitu2.Click += new System.EventHandler(this.btnGehitu2_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(117, 130);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(18, 16);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "Id";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
@@ -191,36 +163,23 @@
             this.label8.TabIndex = 16;
             this.label8.Text = "Id langilea";
             // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(363, 155);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(94, 16);
-            this.label9.TabIndex = 17;
-            this.label9.Text = "Id erabiltzailea";
-            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnGehitu2);
-            this.Controls.Add(this.txtIdErabiltzailea);
             this.Controls.Add(this.txtIdLangilea);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtNan);
             this.Controls.Add(this.txtTel);
             this.Controls.Add(this.txtAbizena);
             this.Controls.Add(this.txtIzena);
-            this.Controls.Add(this.txtId);
             this.Controls.Add(this.btnGehitu);
             this.Controls.Add(this.label1);
             this.Name = "Form2";
@@ -235,21 +194,17 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnGehitu;
-        private System.Windows.Forms.TextBox txtId;
         private System.Windows.Forms.TextBox txtIzena;
         private System.Windows.Forms.TextBox txtAbizena;
         private System.Windows.Forms.TextBox txtTel;
         private System.Windows.Forms.TextBox txtNan;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtIdLangilea;
-        private System.Windows.Forms.TextBox txtIdErabiltzailea;
         private System.Windows.Forms.Button btnGehitu2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
     }
 }
--- DA_Entrega2_Unai_Eta_Gorka_C#/DA_Entrega2_Unai_Eta_Gorka/Form2.cs
+++ DA_Entrega2_Unai_Eta_Gorka_C#/DA_Entrega2_Unai_Eta_Gorka/Form2.cs
@@ -32,21 +32,20 @@ namespace DA_Entrega2_Unai_Eta_Gorka
         {
             string connectionString = "server=localhost;port=3306;database=entrega2_da;user=root;password=1WMG2023;";
 
-            string id = txtId.Text;
             string izena = txtIzena.Text;
             string abizena = txtAbizena.Text;
             string tel = txtTel.Text;
             string nan = txtNan.Text;
             string arduraduna = "0";
 
-            if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(izena) || string.IsNullOrEmpty(abizena) ||
+            if (string.IsNullOrEmpty(izena) || string.IsNullOrEmpty(abizena) ||
                 string.IsNullOrEmpty(tel) || string.IsNullOrEmpty(nan))
             {
                 MessageBox.Show("Datu guztiak sartu mesedez.");
                 return;
             }
 
-            string query = "INSERT INTO entrega2_da.langilea (id, nan, izena, abizena, arduraduna, telefonoa) VALUES (@id, @nan, @izena, @abizena, @arduraduna, @tel)";
+            string query = "INSERT INTO entrega2_da.langilea (nan, izena, abizena, arduraduna, telefonoa) VALUES (@nan, @izena, @abizena, @arduraduna, @tel)";
 
             using (MySqlConnection conexion = new MySqlConnection(connectionString))
             {
@@ -57,7 +56,6 @@ namespace DA_Entrega2_Unai_Eta_Gorka
                     using (MySqlCommand comando = new MySqlCommand(query, conexion))
                     {
 
-                        comando.Parameters.AddWithValue("@id", id);
                         comando.Parameters.AddWithValue("@nan", nan);
                         comando.Parameters.AddWithValue("@izena", izena);
                         comando.Parameters.AddWithValue("@abizena", abizena);
@@ -90,15 +88,14 @@ namespace DA_Entrega2_Unai_Eta_Gorka
             string connectionString = "server=localhost;port=3306;database=entrega2_da;user=root;password=1WMG2023;";
 
             string id = txtIdLangilea.Text;
-            string id2 = txtIdErabiltzailea.Text;
 
-            if (string.IsNullOrEmpty(id) || string.IsNullOrEmpty(id2))
+            if (string.IsNullOrEmpty(id))
             {
                 MessageBox.Show("Datu guztiak sartu mesedez.");
                 return;
             }
 
-            string query = "INSERT INTO entrega2_da.erabiltzailea (id_erabiltzailea, id_langilea) VALUES (@id, @id2)";
+            string query = "INSERT INTO entrega2_da.erabiltzailea (id_langilea) VALUES (@id)";
 
             using (MySqlConnection conexion = new MySqlConnection(connectionString))
             {
@@ -110,7 +107,6 @@ namespace DA_Entrega2_Unai_Eta_Gorka
                     {
 
                         comando.Parameters.AddWithValue("@id", id);
-                        comando.Parameters.AddWithValue("@id2", id2);
                        
                         int emaitza = comando.ExecuteNonQuery();
 
@@ -142,8 +138,7 @@ namespace DA_Entrega2_Unai_Eta_Gorka
                         {
 
                             comando.Parameters.AddWithValue("@id", id);
-                            comando.Parameters.AddWithValue("@id2", id2);
-
+                       
                             int emaitza = comando.ExecuteNonQuery();
 
                             if (emaitza > 0)
